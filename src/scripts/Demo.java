2a6e2b1a5cc82cd8ff7ad5a648c7b1cb40e7771f
Qa_lead <mayookhar3@gmail.com>
2026-03-24 14:27:39 +0530

    Adding again the modification to the existing script

--- src/scripts/Demo.java
+++ src/scripts/Demo.java
@@ -5,6 +5,8 @@ public static void main(String[] args) {
 	System.out.println("first class of git");
 	System.out.println("first class of git");
 	System.out.println("first class of git");
+	System.out.println("first class of git1");
+	System.out.println("first class of git3");
 	
 }
 }
