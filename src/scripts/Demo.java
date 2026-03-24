39a74195c4b608f1db8a49a447f42a83da2ac62d
Qa_lead <mayookhar3@gmail.com>
2026-03-24 14:24:32 +0530

    Adding same script after modification

--- src/scripts/Demo.java
+++ src/scripts/Demo.java
@@ -3,5 +3,8 @@ package scripts;
 public class Demo {
 public static void main(String[] args) {
 	System.out.println("first class of git");
+	System.out.println("first class of git");
+	System.out.println("first class of git");
+	
 }
 }
